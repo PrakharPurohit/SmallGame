e59dcef334efb39463595ffd425a282792a2a817
PrakharPurohit <78981508+PrakharPurohit@users.noreply.github.com>
2021-02-13 01:12:25 +0530

    C Code

---
+++ Project1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int guess, number, nguess=0 ;
+    srand(time(0));
+    number = rand() % 100 + 1;
+    // printf("The number is %d" ,number);
+    
+
+    do
+    {
+        printf("Guess the number between 1 to 100 ! \n");
+        scanf("%d", &guess);
+
+        if (guess > number)
+        {
+            printf("Guess Lower number \n");
+        }
+        else if (guess < number)
+        {
+            printf("guess Higher number\n ");
+        }
+        else
+        {
+            printf("You guessed it in %d attempts \n", nguess);
+        }
+        nguess++;
+    } while (guess != number);
+
+    return 0;
+}
